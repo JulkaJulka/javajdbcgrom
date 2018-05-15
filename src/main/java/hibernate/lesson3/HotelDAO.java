415f35253c0a9cfa7d32be3d02c79dcde90b6680
Juli <walarm@meta.ua>
2018-05-15 11:08:27 +0300

    oneToOne

--- src/main/java/hibernate/lesson3/HotelDAO.java
+++ src/main/java/hibernate/lesson3/HotelDAO.java
@@ -1,9 +1,18 @@
 package hibernate.lesson3;
 
 
+import org.hibernate.HibernateException;
+
 public class HotelDAO extends GeneralDao<Hotel> {
 
     public static final String FIND_HT_BY_ID_HOTEL = "FROM Hotel WHERE ID = :ID ";
     public static final String DELETE_HT_BY_ID_HOTEL = "DELETE FROM Hotel WHERE ID = :ID";
 
+    public Hotel delete(long id){
+        return delete(DELETE_HT_BY_ID_HOTEL, id);
+    }
+
+    public Hotel findById( long id) throws HibernateException {
+        return findById(FIND_HT_BY_ID_HOTEL, id);
+    }
 }
--- src/main/java/hibernate/lesson3/RoomDAO.java
+++ src/main/java/hibernate/lesson3/RoomDAO.java
@@ -1,10 +1,19 @@
 package hibernate.lesson3;
 
+import org.hibernate.HibernateException;
+
 public class RoomDAO extends GeneralDao<Room> {
 
     public static final String FIND_RM_BY_ID_ROOM = "FROM Room WHERE ID = :ID ";
     public static final String DELETE_RM_BY_ID_ROOM = "DELETE FROM Room WHERE ID = :ID ";
 
+    public Room delete(long id){
+        return delete(DELETE_RM_BY_ID_ROOM, id);
+    }
+
+    public Room findById( long id) throws HibernateException {
+        return findById(FIND_RM_BY_ID_ROOM, id);
+    }
 
 }
 
